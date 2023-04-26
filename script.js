b118005288993f5cd3c8fd03c88e409dc13ed5c7
Ben Spooner <113096080+unwontedsponn@users.noreply.github.com>
2023-04-26 13:20:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -322,26 +322,7 @@ span7.onclick = function() {
     modal7.style.display = "none";
 }
 
-// MODAL FOR CV2---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Get the modal
-const modal8 = document.getElementById("cv2Modal");
-
-// Get the cv1 link that opens the modal
-const cv2 = document.getElementById("cv2");
-
-// When the user clicks on the cv1 link, open the modal 
-cv2.onclick = function() {
-    modal8.style.display = "block";
-}
-
-// Get the <span> element that closes the modal
-const span8 = document.getElementsByClassName("close8")[0];
-// When the user clicks on <span> (x), close the modal
-span8.onclick = function() {
-    modal8.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modals, close it
+// When the user clicks anywhere outside of the modals, close it-------------------------------------------------
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
