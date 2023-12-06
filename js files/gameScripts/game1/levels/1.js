1abe5fd2cbc782979987485c6f36edbe0c4e43b9
Ben Spooner <113096080+unwontedsponn@users.noreply.github.com>
2023-12-06 13:42:02 +0000

    Add files via upload

--- js files/gameScripts/game1/levels/1.js
+++ js files/gameScripts/game1/levels/1.js
@@ -15,27 +15,11 @@ export function levelOne() {
     createEdibles(edibles);
     createEnemies(enemies, gameSettings.fInterval);
 
-    // Fade title text over 2 seconds
+    // Fade title text over 4 seconds
     fadeText('game-title', 2000, 0);
 
-    // Fading out instructions over 2 seconds, and then call try-not-to-die text
-    fadeText('large-p-game1', 2000, 0, () => {
-        
-        // If player hits an enemy, immediately fade try-not-to-die instructions to 0 opacity
-        if (gameSettings.level === 0) fadeText('try-not-to-die', 0, 0);
-
-        // Fade in 'try not to die' instructions over 1 second
-        else if (gameSettings.level === 1 && gameSettings.timesPlayedTheGame === 0) {
-
-            fadeText('try-not-to-die', 2000, 0.2);
-
-            // Fade out "try not to die" after 1 second
-            setTimeout(() => {
-                fadeText('try-not-to-die', 1000, 0);
-            }, 2000);
-
-        }
-    });
+    // Fading out instructions over 2 seconds
+    fadeText('large-p-game1', 2000, 0);
 
     // POWER UP - SCORE TEXT
     if (powerUpCollected) {
